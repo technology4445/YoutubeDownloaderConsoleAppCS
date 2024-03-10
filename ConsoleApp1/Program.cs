a340ba75996293b40f7c5e3856cb0cedff573292
Muhammad Abdullah <am345833@gmail.com>
2024-03-10 19:34:38 +0500

    Console App YTD
    Console App YTD

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using YoutubeExplode;
+
+class Program
+{
+    static async Task Main()
+    {
+        // Replace "VIDEO_URL" with the URL of the YouTube video you want to download
+        //string videoUrl = "https://www.youtube.com/watch?v=fxDIyALaOI0";
+        Console.WriteLine("Enter Url...");
+        string videoUrl = Console.ReadLine().ToString();
+
+        var youtube = new YoutubeClient();
+        var video = await youtube.Videos.GetAsync(videoUrl);
+
+        if (video != null)
+        {
+            var streamInfoSet = await youtube.Videos.Streams.GetManifestAsync(video.Id);
+            var streamInfo = streamInfoSet.GetMuxedStreams().FirstOrDefault();
+
+            if (streamInfo != null)
+            {
+                var videoStream = await youtube.Videos.Streams.GetAsync(streamInfo);
+                Console.WriteLine("enter output");
+
+                string o = Console.ReadLine();
+                string outputFilePath = Path.Combine(o);
+                //string outputFilePath = Path.Combine(o, $"{video.Title}.{videoStream.Container.Name}");
+
+                using (var fileStream = File.Create(outputFilePath))
+                {
+                    await videoStream.CopyToAsync(fileStream);
+                    Console.WriteLine($"Video downloaded to: {outputFilePath}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No suitable video stream found.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Video not found.");
+        }
+    }
+}
